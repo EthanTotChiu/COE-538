e32b83a2bf20ee45e796f5048b45c00c17f664e5
Ethan Chiu <chiu.ethan@outlook.com>
2024-10-04 13:07:17 -0400

    Subroutine

--- Lab3/Sources/main.asm
+++ Lab3/Sources/main.asm
@@ -76,20 +76,101 @@ msg1        dc.b    "Battery volt ",0
 msg2        dc.b    "Sw status ",0
             
 ; Subroutine section
-initLCD ...
-clrLCD ...
-del_50us ...
-cmd2LCD ...
-putsLCD ...
-putcLCD ...
-dataMov ...
+;*******************************************************************
+;* Initialization of the LCD: 4-bit data width, 2-line display,    *
+;* turn on display, cursor and blinking off. Shift cursor right.   *
+;*******************************************************************
+initLCD       BSET    DDRS,%11110000     ; configure pins PS7,PS6,PS5,PS4 for output
+              BSET    DDRE,%10010000     ; configure pins PE7,PE4 for output
+              LDY     #2000              ; wait for LCD to be ready
+              JSR     del_50us           ; -"-
+              LDAA    #$28               ; set 4-bit data, 2-line display
+              JSR     cmd2LCD            ; -"-
+              LDAA    #$0C               ; display on, cursor off, blinking off
+              JSR     cmd2LCD            ; -"-
+              LDAA    #$06               ; move cursor right after entering a character
+              JSR     cmd2LCD            ; -"-
+              RTSclrLCD ...
+;*******************************************************************
+;* ([Y] x 50us)-delay subroutine. E-clk=41,67ns. *
+;*******************************************************************
+del_50us:     PSHX                ;2 E-clk
+eloop:        LDX   #30           ;2 E-clk -
+iloop:        PSHA                ;2 E-clk |
+              PULA                ;3 E-clk |
+              PSHA                ;2 E-clk | 50us
+              PULA                ;3 E-clk |
+              PSHA                ;2 E-clk |
+              PULA                ;3 E-clk |
+              PSHA                ;2 E-clk |
+              PULA                ;3 E-clk |
+              PSHA                ;2 E-clk |
+              PULA                ;3 E-clk |
+              PSHA                ;2 E-clk |
+              PULA                ;3 E-clk |
+              NOP                 ;1 E-clk |
+              NOP                 ;1 E-clk |
+              DBNE  X,iloop       ;3 E-clk -
+              DBNE  Y,eloop       ;3 E-clk
+              PULX                ;3 E-clk
+              RTS                 ;5 E-clkcmd2LCD ...
+;*******************************************************************
+;* This function outputs a NULL-terminated string pointed to by X *
+;*******************************************************************
+putsLCD       LDAA  1,X+          ; get one character from the string
+              BEQ   donePS        ; reach NULL character?
+              JSR   putcLCD
+              BRA   putsLCD
+donePS        RTS
+;*******************************************************************
+;* This function outputs the character in accumulator in A to LCD *
+;*******************************************************************
+putcLCD       BSET  LCD_CNTR,LCD_RS  ; select the LCD Data register (DR)
+              JSR   dataMov           ; send data to DR
+              RTS
+;*******************************************************************
+;* This function sends data to the LCD IR or DR depening on RS *
+;*******************************************************************
+dataMov       BSET    LCD_CNTR,LCD_E   ; pull the LCD E-sigal high
+              STAA    LCD_DAT          ; send the upper 4 bits of data to LCD
+              BCLR    LCD_CNTR,LCD_E   ; pull the LCD E-signal low to complete the write oper.
+              LSLA                     ; match the lower 4 bits with the LCD data pins
+              LSLA                     ; -"-
+              LSLA                     ; -"-
+              LSLA                     ; -"-
+              BSET    LCD_CNTR,LCD_E   ; pull the LCD E signal high
+              STAA    LCD_DAT          ; send the lower 4 bits of data to LCD
+              BCLR    LCD_CNTR,LCD_E   ; pull the LCD E-signal low to complete the write oper.
+              LDY     #1               ; adding this delay will complete the internal
+              JSR     del_50us         ; operation for most instructions
+              RTS
+
+           
 int2BCD ...
-BCD2ASC ...
-initAD      MOVB    #$C0,ATDCTL2      ;power up AD, select fast flag clear
-            JSR     del_50us          ;wait for 50 us
-            MOVB    #$00,ATDCTL3      ;8 conversions in a sequence
-            MOVB    #$85,ATDCTL4      ;res=8, conv-clks=2, prescal=12
-            BSET    ATDDIEN,$0C       ;configure pins AN03,AN02 as digital inputs
-            RTS
-; Interrupt vectors
-...
+
+
+;*******************************************************************
+;* Binary to ASCII *
+;*******************************************************************
+leftHLF       LSRA                    ; shift data to right
+              LSRA
+              LSRA
+              LSRA
+rightHLF      ANDA    #$0F             ; mask top half
+              ADDA    #$30             ; convert to ascii
+              CMPA    #$39
+              BLE     out               ; jump if 0-9
+              ADDA    #$07             ; convert to hex A-F
+out           RTS
+
+initAD        MOVB    #$C0,ATDCTL2      ;power up AD, select fast flag clear
+              JSR     del_50us          ;wait for 50 us
+              MOVB    #$00,ATDCTL3      ;8 conversions in a sequence
+              MOVB    #$85,ATDCTL4      ;res=8, conv-clks=2, prescal=12
+              BSET    ATDDIEN,$0C       ;configure pins AN03,AN02 as digital inputs
+              RTS
+;********************************************************************
+;* Interrupt Vectors                                                *
+;********************************************************************
+              ORG $FFFE
+              FDB Entry ; Reset Vector
